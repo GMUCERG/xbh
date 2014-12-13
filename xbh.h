5f739efa014e89b7e42ebf6387f78053ca5ea258
John Pham <jhnphm@gmail.com>
2014-12-13 11:50:05 -0500

    Get cpu clockspeed from g_syshz

--- xbh.h
+++ xbh.h
@@ -1,7 +1,9 @@
 #ifndef _XBH_H
 #define _XBH_H
-#define LWIP_XBH_DEBUG LWIP_DBG_ON
-#define CYCLES_PER_US ((F_CPU+500000)/1000000)
+
+extern uint32_t g_syshz;
+#define CYCLES_PER_US (g_syshz/1000000)
+
 
 
 uint32_t XBH_handle(const uint8_t *input, uint16_t input_len, uint8_t* output);
