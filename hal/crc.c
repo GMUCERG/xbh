60b28723bd2f3c09352bd25ffd89ffd7809ec65e
John Pham <jhnphm@gmail.com>
2015-01-01 07:24:50 -0500

    Added wrapper for CRC implementation from driverlib in ROM

---
+++ hal/crc.c
@@ -0,0 +1,3 @@
+#include "hal/crc.h"
+// Required to compile inline w/ c99
+extern inline uint16_t crc16_create(void *buf, size_t len);
--- hal/crc.h
+++ hal/crc.h
@@ -1,6 +1,16 @@
-#ifndef _CRC_H
-#define _CRC_H
-/**
- * @file Contains wrappers around TI's driverlib crc implementation
- */
+#ifndef _CRC_H_
+#define _CRC_H_
+#include <inttypes.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+#include <driverlib/rom.h>
+#include <driverlib/rom_map.h>
+
+#include <driverlib/sw_crc.h>
+
+inline uint16_t crc16_create(void *buf, size_t len){
+    return MAP_Crc16(0, buf, len);
+}
+
 #endif
