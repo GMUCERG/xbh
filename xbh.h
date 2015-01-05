ba8b6957f7f006cc1c468976ba459c50ac1bed21
John Pham <jhnphm@gmail.com>
2015-01-05 01:37:11 -0500

    Moved defines from xbh.h to xbh_xbdcomm.h

--- xbh.h
+++ xbh.h
@@ -13,15 +13,6 @@
 size_t XBH_handle(int sock, const uint8_t *input, size_t input_len, uint8_t *reply);
 
 
-#define ADDRSIZE sizeof(uint32_t) //bytes used to store address 
-#define LENGSIZE sizeof(uint32_t) //bytes used to store length
-#define SEQNSIZE sizeof(uint32_t) //bytes used to store sequence counter
-#define TYPESIZE sizeof(uint32_t)
-#define TIMESIZE sizeof(uint32_t)
-#define NUMBSIZE sizeof(uint32_t)
-#define REVISIZE 40	//GIT revisions are 40 digit hex numbers
-
-#define CRC16SIZE 2
 
 
 #endif
