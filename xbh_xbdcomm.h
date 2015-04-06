ead9af290ee54407432197b11d1dc7a30e1010f1
John Pham <jhnphm@gmail.com>
2015-04-05 21:09:07 -0400

    Increased XBD answerleng to 48

--- xbh_xbdcomm.h
+++ xbh_xbdcomm.h
@@ -15,15 +15,14 @@ enum XBD_COMM_PROT{COMM_I2C, COMM_UART, COMM_UART_OVERDRIVE, COMM_ETHERNET};
 #define REVISIZE 7	//GIT revisions are 40 digit hex numbers, only use first 7 
 #define CRC16SIZE sizeof(uint16_t)
 
-//TODO: XXX Make sure these buffer sizes do not overflow 
-//
-#define XBD_ANSWERLENG_MAX 32
+//TODO: XXX //Make sure this matches XBD 
+#define XBD_ANSWERLENG_MAX 48
 // Matches max i2c packet size set in atmega ports of XBD
-#define XBD_PACKET_SIZE_MAX (160)
+#define XBD_PACKET_SIZE_MAX 160
 #define XBD_PKT_PAYLOAD_MAX (XBD_PACKET_SIZE_MAX-XBD_COMMAND_LEN-CRC16SIZE)
 
 
-#define XBD_RESULTLEN_EBASH (XBD_COMMAND_LEN+1+2+CRC16SIZE)
+//#define XBD_RESULTLEN_EBASH (XBD_COMMAND_LEN+1+2+CRC16SIZE)
 
 void xbdCommInit(uint8_t commMode);
 void xbdCommExit(void);
